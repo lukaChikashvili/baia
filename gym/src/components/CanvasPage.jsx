a41fa0c37a5b5a4a7bb422ab50d6a17be52b88b3
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-18 21:05:30 +0400

    add 3d models

--- gym/src/components/CanvasPage.jsx
+++ gym/src/components/CanvasPage.jsx
@@ -4,7 +4,7 @@ import Lights from './Lights'
 const CanvasPage = () => {
   return (
    <>
-     <Canvas camera={ { fov: 75, near: 0.2, far: 1000, position: [2, 3, 0] } }>
+     <Canvas camera={ { fov: 75, near: 0.2, far: 1000, position: [4, 2, -2] } }>
         <Lights />
         <Experience />
      </Canvas>
--- gym/src/components/Experience.jsx
+++ gym/src/components/Experience.jsx
@@ -3,12 +3,16 @@ import { OrbitControls, useGLTF } from '@react-three/drei'
 
 const Experience = () => {
     const model = useGLTF('./gym.glb');
+    const floor = useGLTF('./floor.glb');
 
   return (
    <>
    <OrbitControls makeDefault />
    
-   <primitive object={model.scene}  scale = {100}/>
+   <primitive object={model.scene}  scale = {0.5} position = {[1, 0, 1.5]} />
+   <primitive object={floor.scene}/>
+
+
      
    </>
   )
