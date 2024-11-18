c797e91bcf283238d83c0ee0b7af90be3d529dbc
luka chikashvili <lukachikashvili2020@gmail.com>
2024-11-18 22:28:44 +0400

    add pool model

--- gym/src/components/Experience.jsx
+++ gym/src/components/Experience.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react'
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import React, { useEffect, useRef } from 'react'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
 
 
 const Experience = () => {
     const model = useGLTF('./gym.glb');
     const floor = useGLTF('./floor.glb');
+    const pool = useGLTF('./pool.glb');
 
- 
+
 
 
     useEffect(() => {
@@ -15,21 +16,27 @@ const Experience = () => {
         if(child.isMesh) {
            if(child.material ) {
             console.log(child.material)
-             child.material.color = new THREE.Color('#e4efe9');
-             child.material.emissive  = new THREE.Color('#e4efe9');
-             child.material.emissiveIntensity = 0.6;
+             child.material.color = new THREE.Color('#886AEA');
+             child.material.emissive  = new THREE.Color('#886AEA');
+             child.material.emissiveIntensity = 0.3;
              child.material.roughness = 0.3;
            }
         }
-       })
+       });
+
+ 
     }, [floor]);
 
   return (
    <>
    <OrbitControls makeDefault />
    
+   
    <primitive object={model.scene}  scale = {0.5} position = {[1, 0, 1.5]} castshadow/>
    <primitive object={floor.scene}/>
+
+   <primitive object={pool.scene} scale = {0.12} position = {[-1, 0.5, -3.5]} />
+   
 
 
 
